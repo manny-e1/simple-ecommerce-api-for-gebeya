2a30c4ca417a29b8e9a1662ff645826d94775b7d
Amanuel Ewnetu <aewnetu6@gmail.com>
2020-11-27 00:49:11 +0300

    auth middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+const protect = asyncHandler( async (req,res,next) => {
+    let token 
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            const {id} = jwt.verify(token, process.env.SECRET_KEY)
+            const currentUser = await User.findById(id)
+            req.user = currentUser
+            next()
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error('Not Authorized')
+        }
+    }
+    if (!token) {
+        res.status(401)
+        throw new Error('Not authorized, no token')
+      }
+})
+
+export default protect
