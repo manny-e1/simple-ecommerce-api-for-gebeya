a898782a6c6f71eabe58519b4119eaa1923c04ff
Amanuel Ewnetu <aewnetu6@gmail.com>
2020-11-26 23:32:46 +0300

    server creation

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (_,res) => {
+    res.json({message:"heynpm"})
+})
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`server listening on port ${port}`))
