fb184cb92c8db372188df68edda0f40fd777854d
Amanuel Ewnetu <aewnetu6@gmail.com>
2020-11-27 01:15:21 +0300

    get users and getuserbyid

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -47,3 +47,24 @@ export const userLogin = asyncHandler(async (req,res) =>{
 })
 
 
+export const getUsers = asyncHandler(async (req,res) =>{
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(404)
+        throw new Error('Error retriving users!')
+    }
+})
+
+export const getUserById = asyncHandler(async (req,res) =>{
+    try {
+        const {id} = req.params
+        const user = await User.findById(id)
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404)
+        throw new Error('User not found!')
+    }
+})
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { userRegister, userLogin } from '../controllers/userControllers.js'
+import { userRegister, userLogin, getUsers, getUserById } from '../controllers/userControllers.js'
 import protect from '../middlewares/authentication.js'
 
 const router = express.Router()
 
 router.route('/register').post(protect, userRegister)
 router.route('/login').post(userLogin)
+router.route('/all').get(protect, getUsers)
+router.route('/:id').get(protect, getUserById)
 
 export default router
