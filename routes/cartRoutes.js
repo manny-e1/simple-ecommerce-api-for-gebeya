70fda1f0db3b0f442dab896d6dd64164a3b21111
Amanuel Ewnetu <aewnetu6@gmail.com>
2020-11-27 13:39:41 +0300

    get carts

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addToCart } from '../controllers/cartControllers.js'
+import { addToCart, getCarts } from '../controllers/cartControllers.js'
 import protect from '../middlewares/authentication.js'
 
 const router = express.Router()
 
 router.route('/addToCart').post(protect, addToCart)
+router.route('/').get(protect, getCarts)
 
 export default router
