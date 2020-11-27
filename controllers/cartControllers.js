55b9ce07d91312e346dd4be53f603410b271875f
Amanuel Ewnetu <aewnetu6@gmail.com>
2020-11-27 13:40:26 +0300

    delete cart

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -43,3 +43,61 @@ export const addToCart = asyncHandler( async (req,res) => {
         res.status(200).json(userCart)
     
 })
+
+export const getCarts = asyncHandler ( async (req,res) => {
+    try {
+        const cartArray = []
+        let cartProductsArray = []
+        const carts = await Cart.find().populate({
+            path: 'products',
+            populate: {
+              path: 'product',
+              model: 'Product',
+              select: "name price countInStock description image",
+              populate: {
+                path: 'vendor',
+                model: 'User',
+                select: "name email"
+              },
+            },
+          })
+        carts.forEach(async prod => {
+            prod.products.forEach(async (item) =>{
+                cartProductsArray.push({
+                    product: item.product,
+                    quantity: item.quantity,
+                    subTotalPrice: item.product.price * item.quantity
+                }) 
+                
+            })
+            console.log(cartProductsArray);
+        })
+        let totalPrice = 0
+        cartProductsArray.forEach(prod =>     
+            totalPrice += prod.subTotalPrice )
+        
+        carts.forEach(cart => {
+            cartArray.push({
+                id:cart._id,
+                buyer: cart.buyer,
+                products: cartProductsArray,
+                totalPrice
+            })
+        })
+        res.status(200).json(cartArray)
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error getting carts")
+    }
+})
+
+export const deleteCart = asyncHandler (async (req,res) => {
+    try {
+        const deletedCart = await Cart.findByIdAndDelete(req.params.id)
+    res.json(deletedCart)
+    } catch (error) {
+        throw new Error("Error deleting cart")
+    }
+    
+})
+
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addToCart, getCarts } from '../controllers/cartControllers.js'
+import { addToCart, getCarts, deleteCart} from '../controllers/cartControllers.js'
 import protect from '../middlewares/authentication.js'
 
 const router = express.Router()
 
 router.route('/addToCart').post(protect, addToCart)
 router.route('/').get(protect, getCarts)
+router.route('/delete/:id').delete(protect, deleteCart)
 
 export default router
